240090d2ade0765162fe896db1b6aff4b55671f6
hfikry92 <50300462+hfikry92@users.noreply.github.com>
2019-07-21 18:35:35 +1000

    version number update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TinderBot",
-    version="0.0.1",
+    version="0.0.2",
     author="Hosam Fikry",
     author_email="hfikry92@gmail.com",
     description="A small example package",
